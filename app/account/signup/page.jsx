c34450d4ce89396fd07a7870da48711bd4e5c2e6
junaidjesan2 <junaidjesan2@gmail.com>
2024-03-31 16:32:13 +0600

    sign in and up form created

--- app/account/signup/page.jsx
+++ app/account/signup/page.jsx
@@ -1,10 +1,39 @@
-import Image from 'next/image'
-import Construction from "../../../public/banner/construction.svg"
-
 export default function page() {
   return (
-    <div>
-        <Image alt='' className='w-1/2 mx-auto h-fit' src={Construction}/>
+    <div className="min-h-screen lg:mt-28">
+      <div className="flex flex-col justify-center items-center space-y-5 bg-stone-400 py-28 rounded-xl">
+        <h1 className="text-3xl font-semibold">Sign Up</h1>
+        <form action="" className="flex flex-col space-y-5 w-3/6">
+          <div className="flex flex-col-reverse">
+            <input
+              className="py-2 px-2 rounded-lg"
+              type="text"
+              id="email"
+              placeholder="email"
+            />
+            <label htmlFor="email">Email</label>
+          </div>
+          <div className="flex flex-col-reverse">
+            <input
+              className="py-2 px-2 rounded-lg"
+              type="text"
+              id="password"
+              placeholder="Password"
+            />
+            <label htmlFor="password">Password</label>
+          </div>
+          <div className="flex flex-col-reverse">
+            <input
+              className="py-2 px-2 rounded-lg"
+              type="text"
+              id="repassword"
+              placeholder="Retype Password"
+            />
+            <label htmlFor="repassword">Retype Password</label>
+          </div>
+          <button className="py-2 rounded-lg bg-neutral-600">Sign Up</button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
